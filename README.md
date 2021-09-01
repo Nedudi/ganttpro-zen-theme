07521ab825550b01e1e7bd79f179dc23b7b530d9
Igor Kovalyonok <igor@ganttpro.com>
2021-09-01 15:36:54 +0300

    Init project

---
+++ README.md
@@ -0,0 +1,43 @@
+**!!! Перед деплоем собрать тему `npm run build`**
+
+## Команды
+
+**Для разработки:**
+
+`npm run dev` - _сборка стилей и скриптов, отлеживать их изменение_
+
+`zat theme preview` - _запуск Zendesk App Tools для локального предпросмотра темы_
+
+**Перед деплоем**
+
+`npm run build` - _сборка стилей и скриптов (минифицируются, удалятся sourcemap)_
+
+## Базовая структура
+
+- /src
+  - /css
+    - /general - _глобальные/основыные стили_
+      - \_сopenhagen.css - _стили родительской темы_
+    - /blocks - _БЭМ-блоки_
+    - style.css - _тут импортировать стили_
+  - js - _скрипты, при создании новых, для сбоки подключить в gulpfile.js_
+    - сopenhagen.js - _скрипты родительской темы_
+  - style.css, script.js - _скомпилированные стили и скрипты_
+
+**Примечание**
+
+- [Установка ZAT в Windows 10](https://develop.zendesk.com/hc/en-us/community/posts/360037023114--Tutorial-ZAT-installation-on-Windows-10)
+- При запуске `zat theme preview`, что бы каждый раз не вводить данные аутентификации, в корне темы создать файл .zat (см. ниже)
+- Настройки темы, а так же переменные для CSS задаются в файле manifest.json
+- CSS-прероцессор не используется из-за особенностей ZAT
+- При написании CSS, руководствоваться [БЭМ - Быстрый старт](https://ru.bem.info/methodology/quick-start/)
+
+Содержимое .zat файла:
+
+```
+{
+ "subdomain": "your_subdomain",
+ "username": "your_username",
+ "password": "your_password"
+}
+```
---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+const { src, dest, series, watch } = require('gulp');
+const plumber = require('gulp-plumber');
+const gulpif = require('gulp-if');
+const concat = require('gulp-concat');
+const concatCss = require('gulp-concat-css');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
+
+const CSS_PATH = './src/css/';
+const JS_PATH = './src/js/';
+
+const mode = process.env.NODE_ENV;
+
+function styles(cb) {
+  return src(CSS_PATH + 'style.css')
+    .pipe(plumber())
+    .pipe(
+      concatCss('./style.css', {
+        rebaseUrls: false,
+      })
+    )
+    .pipe(postcss([autoprefixer()]))
+    .pipe(gulpif(mode === 'prod', cleanCSS()))
+    .pipe(dest('./'));
+
+  cb();
+}
+
+function javascript(cb) {
+  return src([JS_PATH + 'сopenhagen.js'])
+    .pipe(plumber())
+    .pipe(gulpif(mode === 'dev', sourcemaps.init()))
+    .pipe(concat('script.js'))
+    .pipe(babel({ presets: ['@babel/env'] }))
+    .pipe(gulpif(mode === 'dev', sourcemaps.write()))
+    .pipe(gulpif(mode === 'prod', uglify()))
+    .pipe(dest('./'));
+
+  cb();
+}
+
+function track(cb) {
+  if (mode === 'prod') return cb();
+
+  watch(CSS_PATH + '**/*.css').on('change', series(styles));
+  watch(JS_PATH + '**/*.js').on('change', series(javascript));
+}
+
+exports.default = series(styles, javascript, track);
