57b8e821cdcaa53250c2aaa8ecd5cc9f6f1df2f5
Igor Kovalyonok <igor@ganttpro.com>
2022-05-05 17:57:31 +0300

    added live chat and GTM

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,7 @@ function styles(cb) {
 }
 
 function javascript(cb) {
-  return src([JS_PATH + 'theme.js', JS_PATH + 'main.js', JS_PATH + 'navigation.js'])
+  return src([JS_PATH + 'theme.js', JS_PATH + 'main.js', JS_PATH + 'navigation.js', JS_PATH + 'live-chat.js'])
     .pipe(plumber())
     .pipe(gulpif(mode === 'dev', sourcemaps.init()))
     .pipe(concat('script.js'))
---
+++ src/js/live-chat.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+  window.__lc = window.__lc || {};
+  window.__lc.license = 8254491;
+  // window.__lc.group = !{APPCONFIG.locales.supported[LOCALE].liveChatId};
+
+  (function () {
+    var lc = document.createElement('script');
+    lc.async = true;
+    lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(lc, s);
+  })();
+
+  var LC_API = LC_API || {};
+
+  window.LC_API = LC_API;
+  LC_API.on_after_load = function () {
+    LC_API.disable_sounds();
+  };
+
+  document.querySelectorAll('#openLiveChat').forEach(function (item) {
+    if (item) {
+      item.addEventListener('click', function () {
+        LC_API.open_chat_window();
+      });
+    }
+  });
+});
